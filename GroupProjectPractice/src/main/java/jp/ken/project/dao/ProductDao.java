d019d0fc1adb0ebef2e75018555336d8b79e57e9
student <student@NGY-F17>
2024-12-10 10:05:12 +0900

    セール品

--- GroupProjectPractice/src/main/java/jp/ken/project/dao/ProductDao.java
+++ GroupProjectPractice/src/main/java/jp/ken/project/dao/ProductDao.java
@@ -1,5 +1,24 @@
 package jp.ken.project.dao;
 
-public class ProductDao {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import jp.ken.project.model.ProductModel;
+
+public class ProductDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private RowMapper<ProductModel> productMapper = new BeanPropertyRowMapper<ProductModel>(ProductModel.class);
+
+	public List<ProductModel> getList(){
+		String sql = "SELECT discnt_rate FROM t_products WHERE discnt_is_valid = ?";
+		Object[] parameters = {'1'};
+		List<ProductModel> membersList = jdbcTemplate.query(sql,parameters,productMapper);
+		return membersList;
+	}
 }
