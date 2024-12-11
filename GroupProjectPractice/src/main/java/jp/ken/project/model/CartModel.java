5019b917bebabe1b66a6be0a360f99b14fff8cac
student <student@NGY-F13>
2024-12-11 14:01:52 +0900

    カートモジュールの修正版

--- GroupProjectPractice/src/main/java/jp/ken/project/model/CartModel.java
+++ GroupProjectPractice/src/main/java/jp/ken/project/model/CartModel.java
@@ -8,6 +8,8 @@ public class CartModel implements Serializable{
 	private int price;
 	private int count;
 	private String image;
+	private String discnt_is_valid;							//割引フラグ
+	private Float discnt_rate;					//割引率
 
 	public int getProduct_id() {
 		return product_id;
@@ -38,6 +40,18 @@ public class CartModel implements Serializable{
 	}
 	public void setImage(String image) {
 		this.image = image;
+	}
+	public String getDiscnt_is_valid() {
+		return discnt_is_valid;
+	}
+	public void setDiscnt_is_valid(String discnt_is_valid) {
+		this.discnt_is_valid = discnt_is_valid;
+	}
+	public Float getDiscnt_rate() {
+		return discnt_rate;
+	}
+	public void setDiscnt_rate(Float discnt_rate) {
+		this.discnt_rate = discnt_rate;
 	}
 
 
