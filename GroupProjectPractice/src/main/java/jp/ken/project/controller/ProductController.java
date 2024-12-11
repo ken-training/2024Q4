6c3b1076045a752c3c8b5bde5f50a6915fe1cb87
student <student@NGY-F13>
2024-12-11 14:00:12 +0900

    商品詳細画面動く形にできたかと思います

--- GroupProjectPractice/src/main/java/jp/ken/project/controller/ProductController.java
+++ GroupProjectPractice/src/main/java/jp/ken/project/controller/ProductController.java
@@ -1,5 +1,6 @@
 package jp.ken.project.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +9,12 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.ken.project.dao.ProductDao;
 import jp.ken.project.model.CartModel;
@@ -23,9 +27,13 @@ public class ProductController {
 	@Autowired
 	private ProductDao ProductDao;
 
+//	@Autowired
+//	private List<CartModel> cartList;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String toProduct(HttpSession session, Model model, HttpServletRequest request,
-			@RequestParam("product_id") int product_id) {
+			@RequestParam("product_id") int product_id,
+			@ModelAttribute("message") String message) {
 //		int product_id = (Integer)request.getAttribute("product_id");
 
 		// -------ひとまずテストデータで入力----------
@@ -35,15 +43,24 @@ public class ProductController {
 		ProductModel pmodel = ProductDao.getProductById(product_id);
 		model.addAttribute("productModel", pmodel);
 
+	    // フラッシュアトリビュートからメッセージを受け取って表示
+	    if (message != null) {
+	        model.addAttribute("message", message);  // メッセージをモデルに追加
+	    }
+
 		return "product";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String addProductToCart(HttpSession session, Model model, HttpServletRequest request) {
+	public String addProductToCart(HttpSession session, Model model, HttpServletRequest request,
+			RedirectAttributes redirectAttributes) {
 
 		@SuppressWarnings("unchecked")
 		List<CartModel> cartList = (List<CartModel>)session.getAttribute("cartList");
 
+		if (cartList == null) {
+		    cartList = new ArrayList<CartModel>();  // cartList が null の場合、空のリストを作成
+		}
 
 		int	quantity = Integer.parseInt(request.getParameter("quantity"));
 		int	product_id = Integer.parseInt(request.getParameter("product_id"));
@@ -52,14 +69,19 @@ public class ProductController {
 			return "redirect:/product";
 		}else {
 			boolean productFound = false;  // 一致する商品があったかどうかのフラグ
-			for (CartModel cartModel : cartList) {
-			    if (cartModel.getProduct_id() == product_id) {
-			        // 一致する商品があった場合
-			        int after_qty = cartModel.getCount() + quantity;
-			        cartModel.setCount(after_qty);
-			        productFound = true;  // 商品が見つかったのでフラグを設定
-			        break;  // 一致する商品が見つかったのでループを抜ける
-			    }
+			int total_cnt = 0;  // 合計商品数
+
+			if(cartList != null) {
+				for (CartModel cartModel : cartList) {
+				    if (cartModel.getProduct_id() == product_id) {
+				        // 一致する商品があった場合
+				        int after_qty = cartModel.getCount() + quantity;
+				        cartModel.setCount(after_qty);
+				        productFound = true;  // 商品が見つかったのでフラグを設定
+//				        break;  // 一致する商品が見つかったのでループを抜ける
+				    }
+				    total_cnt += cartModel.getCount();
+				}
 			}
 
 			if (!productFound) {
@@ -74,11 +96,14 @@ public class ProductController {
 			    addCartModel.setDiscnt_rate(pmodel.getDiscnt_rate());
 			    addCartModel.setCount(quantity);
 
+			    total_cnt += addCartModel.getCount();
+
 			    cartList.add(addCartModel);
 			}
 
 			session.setAttribute("cartList", cartList);
-			return "redirect:/product";
+			redirectAttributes.addFlashAttribute("message", "カートに入れました。カート内の商品は" + total_cnt + "個です。");
+			return "redirect:/product?product_id=" + product_id;
 		}
 	}
 }
